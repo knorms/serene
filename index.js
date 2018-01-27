78d35e0cb90cb6c43251e6bdf36c2255f7d5d449
Katie Normandin <57kkate@gmail.com>
2018-01-27 14:14:32 -0500

    cleaning files further to use with npm connect rather than npm express and socket.io

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var sd = require('standard-deviation')
 
-//Make examples and build directories accessible for index.html
-app.use("/examples", express.static(__dirname + '/examples'));
-app.use("/build", express.static(__dirname + '/build'));
 app.use("/js", express.static(__dirname + '/js'));
-app.use("/media", express.static(__dirname + '/media'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
